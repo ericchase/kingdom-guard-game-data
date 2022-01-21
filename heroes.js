d6a7121f737e0c5af4eed94fd30b13239e3342a5
Chase <>
2022-01-21 05:03:52 -0600

    fix: sort happens on spot, always sort by id first

--- heroes.js
+++ heroes.js
@@ -1,90 +1,120 @@
-let memoizedSorts = new Map();
 let sortedBy = '';
 
 const ascImg = `<img src="./arrow-asc.png" alt="">`;
 const dscImg = `<img src="./arrow-dsc.png" alt="">`;
 
-function setRows(rows) {
-    document
-        .querySelector('#hero-ranks > table > tbody')
-        .innerHTML = rows;
+function removeSortOrder() {
+    const headers = document.querySelectorAll('#hero-ranks thead th')
+    for (const header of headers)
+        header.classList.remove('asc', 'dsc')
+}
+
+function compareId(rowA, rowB) {
+    const [a, b] = [rowA.id, rowB.id].map(_ => Number.parseInt(_));
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+function compareRarity(rowA, rowB) {
+    const rarityPriority = ['SSR', 'SR', 'R', 'N'];
+    const [a, b] = [rowA, rowB]
+        .map(row => row.querySelector(`.rarity`).innerHTML)
+        .map(rarity => rarityPriority.indexOf(rarity));
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+function compareCriteria(rowA, rowB, criteria) {
+    const [a, b] = [rowA, rowB].map(_ => _.querySelector(`.${criteria}`).innerHTML);
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+function sortById(rows, sortDescending) {
+    rows.sort(compareId);
+    if (!sortDescending) rows.reverse();
+}
+
+function sortByRarity(rows, sortDescending) {
+    rows.sort(compareRarity);
+    if (!sortDescending) rows.reverse();
+}
+
+function sortByCriteria(rows, criteria, sortDescending) {
+    rows.sort((a, b) => compareCriteria(a, b, criteria));
+    if (!sortDescending) rows.reverse();
 }
 
 function sortRowsBy(sortBy) {
-    const rows = memoizedSorts.get(sortBy);
     const th = document.querySelector(`#hero-ranks thead th.${sortBy}`);
     const sortDescending = !th.classList.contains('dsc');
-
-    document
-        .querySelectorAll('#hero-ranks thead th')
-        .forEach(th =>
-            th.classList.remove('asc', 'dsc'));
-
-    if (sortDescending) {
-        th.classList.add('dsc');
-        setRows(rows.join(''));
-    } else {
-        th.classList.add('asc');
-        setRows([...rows].reverse().join(''));
-    }
+    removeSortOrder();
+    th.classList.add(sortDescending ? 'dsc' : 'asc');
+    const tbody = document.querySelector('#hero-ranks tbody');
+    const rows = [...tbody.querySelectorAll('tr')];
+    rows.forEach(row => row.remove());
+    sortById(rows, sortDescending);
+    if (sortBy === 'rarity') sortByRarity(rows, sortDescending);
+    else if (sortBy !== 'rank') sortByCriteria(rows, sortBy, sortDescending);
+    rows.forEach(row => tbody.appendChild(row));
 }
 
-function createTable() {
-    const cells = ['Rank', 'Rarity', 'Name', 'Element']
-        .map(_ => `<th class="${_.toLowerCase()}" onclick="sortRowsBy('${_.toLowerCase()}')">
+function toHeader(headerString) {
+    const sortBy = headerString.toLowerCase();
+    return `
+        <th class="${sortBy}" onclick="sortRowsBy('${sortBy}')">
             <div>
-                ${_}
+                ${headerString}
                 <img class="asc" src="./arrow-asc-10px.png" alt="">
                 <img class="dsc" src="./arrow-dsc-10px.png" alt="">
             </div>
-        </th>`)
-        .join('');
+        </th>`
+}
 
+function toRow(heroData, index) {
+    const { rank, name, element, rarity } = heroData;
+    return `
+        <tr id="${index}" class="r${rank} ${rarity} ${element}">
+            <td class="rank">${rank}</td>
+            <td class="rarity">${rarity}</td>
+            <td class="character"><div>
+                <div class="portrait"><img class="lazy" data-src="./heroes/portraits/${name}.png" alt="${name}"></div>
+                <div class="name">${name}</div>
+            </div></td>
+            <td class="element">${element}</td>
+        </tr>`
+}
+
+function createTable(headers, rows) {
     document
         .querySelector('#hero-ranks')
-        .innerHTML = `<table>
-            <thead><tr>${cells}</tr></thead>
-            <tbody></tbody>
-        </table>`;
+        .innerHTML = `
+            <table>
+                <thead><tr>${headers}</tr></thead>
+                <tbody>${rows}</tbody>
+            </table>`;
+
+    document
+        .querySelector('#hero-ranks th:first-child')
+        .classList.add('dsc');
 }
 
-function toRows(heroData) {
-    const { rank, name, element, rarity } = heroData;
-
-    return `<tr class="r${rank} ${rarity} ${element}">
-        <td class="rank">${rank}</td>
-        <td class="rarity">${rarity}</td>
-        <td class="name"><div>
-            <div><img class=".lazy" ata-src="./heroes/portraits/${name}.png" alt="${name}"></div>
-            <div>${name}</div>
-        </div></td>
-        <td class="element">${element}</td>
-    </tr>`
+function loadImage(imageNode) {
+    imageNode.src = imageNode.dataset.src;
+    imageNode.classList.remove('lazy');
 }
 
-function sortRows(rows, sortBy) {
-    return rows.sort((row_a, row_b) => {
-        const [a, b] = [row_a[sortBy], row_b[sortBy]];
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
-    });
-}
-
-function memoizeSortedHeroes(heroList) {
-    memoizedSorts.set('rank', heroList.map(toRows));
-
-    ['rarity', 'name', 'element'].forEach(sortBy => {
-        memoizedSorts.set(
-            sortBy, sortRows(heroList, sortBy).map(toRows)
-        );
-    });
-}
-
-createTable();
+const tableHeaders =
+    ['Rank', 'Rarity', 'Name', 'Element']
+        .map(toHeader)
+        .join('');
 
 fetch('./heroes/data.json')
     .then(response => response.json())
-    .then(memoizeSortedHeroes)
-    .then(() => sortRowsBy('rank'))
+    .then(data => data.map(toRow).join(''))
+    .then(rows => createTable(tableHeaders, rows))
+    .then(() => setupLazyLoading('img.lazy', loadImage))
     .catch(e => console.log(e));
